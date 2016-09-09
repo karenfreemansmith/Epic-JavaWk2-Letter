c02b431c702b493ee11a4644eeb4eeb97b887363
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-09 16:14:10 -0700

    add velocity tempate for photo page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,23 +11,8 @@ public class App {
       return new ModelAndView(new HashMap(), "templates/hello.vtl");
     }, new VelocityTemplateEngine());
 
-    get("/photos", (request, response) ->
-    "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Favorite Photos!</title>" +
-        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-        "<div class='container'>" +
-        "<h1>Favorite Traveling Photos</h1>" +
-        "<ul>" +
-          "<li><img src='/images/foggymountain.jpeg' alt='A photo of a mountain.'></li>" +
-          "<li><img src='/images/rockycoast.jpeg' alt='A photo of a a rocky beach.'></li>" +
-        "</ul>" +
-        "</div>" +
-      "</body>" +
-      "</html>"
-    );
+    get("/photos", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/photos.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
