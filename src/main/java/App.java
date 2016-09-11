61e9c7cd24211838b0d19d03491d74985fdf7dd8
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-10 19:22:00 -0700

    add form and customize letter

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,5 +20,21 @@ public class App {
       model.put("template", "templates/photos.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/form", (request, response) -> {
+      Map<String, Object> model=new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model=new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
